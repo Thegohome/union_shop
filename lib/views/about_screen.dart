3bd97bbd1dd7719caf260ecba80cf128d0487ac5
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-04 16:33:01 +0000

    adjust styling

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -8,6 +8,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -15,26 +16,21 @@ class AboutScreen extends StatelessWidget {
             const SizedBox(height: 40),
             const Text(
               'About Us',
-              style: heroTitle,
+              style: pageTitle,
             ),
             const SizedBox(height: 32),
             Container(
               key: const Key('aboutTextBlock'),
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: const Text(
-                'About us\n'
                 'Welcome to the Union Shop!\n\n'
                 'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
                 'All online purchases are available for delivery or instore collection!\n\n'
                 'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
                 'Happy shopping!\n\n'
                 'The Union Shop & Reception Team',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  height: 1.6,
-                  color: Colors.black,
-                ),
+                textAlign: TextAlign.start,
+                style: pageText,
               ),
             ),
             const SizedBox(height: 40),
