dabee160a1a342ea2ef394faf4010d5259567835
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 03:02:50 +0000

    create collection model

---
+++ lib/models/collection.dart
@@ -0,0 +1,20 @@
+import 'package:union_shop/models/product.dart';
+
+enum CategoryType { all, popular, clothing, merchandise }
+
+class Collection {
+  final String id;
+  final String name;
+  final String description;
+  final List<Product> products;
+  final CategoryType categoryType;
+  final String? image;
+
+  Collection({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.products,
+    required this.categoryType,
+    this.image,
+  });
