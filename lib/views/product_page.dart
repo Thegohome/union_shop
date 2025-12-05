351ddd88578d553ff9445987e43500a58de3dd06
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 10:33:30 +0000

    made stateful

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/views/app_styles.dart';
 import 'package:union_shop/views/common_widgets.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({super.key});
 
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
   void navigateToHome(BuildContext context) {
     Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
   }
