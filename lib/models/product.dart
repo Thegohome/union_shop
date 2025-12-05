d06179ab73574ed73930f9edd508bff67a3abad1
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 00:03:01 +0000

    complete core construction of product class

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,13 +1,27 @@
 class Product {
   final String id;
   final String name;
-  final double
+  final double price;
+  final List<String> images;
+  final String description;
+  final bool isSale;
+  final double? salePrice;
 
   Product({
     required this.id,
     required this.name,
-    required this.description,
     required this.price,
-    required this.options,
-  })
+    required this.images,
+    required this.description,
+    this.isSale = false,
+    this.salePrice,
+  }) {
+    _validatePrices();
+  }
+
+  void _validatePrices() {
+    if (isSale && salePrice != null && salePrice! >= price) {
+      throw ArgumentError('Sale price must be less than regular price');
+    }
+  }
 }
