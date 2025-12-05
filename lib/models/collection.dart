6c3a5e50bc9a6af4bfe2ee3bb7375c73e47f7954
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 03:10:57 +0000

    remove references to categories

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,13 +1,10 @@
 import 'package:union_shop/models/product.dart';
 
-enum CategoryType { all, popular, clothing, merchandise }
-
 class Collection {
   final String id;
   final String name;
   final String description;
   final List<Product> products;
-  final CategoryType categoryType;
   final String? image;
 
   Collection({
@@ -15,7 +12,6 @@ class Collection {
     required this.name,
     required this.description,
     required this.products,
-    required this.categoryType,
     this.image,
   });
 
