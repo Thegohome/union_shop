01fab690c328d4c0d04353281fffcb2354b77462
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 03:23:48 +0000

    add JSON serialization methods

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -16,3 +16,37 @@ class Collection {
   });
 
   int get productCount => products.length;
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'products': products.map((product) => product.toJson()).toList(),
+      'image': image,
+    };
+  }
+  
+  factory Collection.fromJson(Map<String, dynamic> json) {
+    return Collection(
+        id: json['id'] as String,
+        name: json['name'] as String,
+        description: json['description'] as String,
+        products: json['products'] != null
+            ? _parseProducts(json['products'])
+            : [],
+        image: json['image'] as String?);
+  }
+
+  static List<Product> _parseProducts(dynamic productsData) {
+    List productList = productsData as List;
+    List<Product> parsedProducts = [];
+
+    for (var productData in productList) {
+      Map<String, dynamic> productMap = productData as Map<String, dynamic>;
+      Product product = Product.fromJson(productMap);
+      parsedProducts.add(product);
+    }
+
+    return parsedProducts;
+  }
+}
