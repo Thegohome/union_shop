dae61f4a470cd8b28750a68e9b9f8a38e7ace6f5
Nickyyl Sharma <94190543+Thegohome@users.noreply.github.com>
2025-12-05 09:40:48 +0000

    update collection to accept IDs instead of whole products as a parameter

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,74 +1,49 @@
-import 'package:union_shop/models/product.dart';
-
 class Collection {
   final String id;
   final String name;
   final String description;
-  final List<Product> products;
+  final List<String> productIds;
   final String? image;
 
   Collection({
     required this.id,
     required this.name,
     required this.description,
-    required this.products,
+    required this.productIds,
     this.image,
   });
 
-  int get productCount => products.length;
+  int get productCount => productIds.length;
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
       'description': description,
-      'products': products.map((product) => product.toJson()).toList(),
+      'productIds': productIds,
       'image': image,
     };
   }
-  
+
   factory Collection.fromJson(Map<String, dynamic> json) {
     return Collection(
         id: json['id'] as String,
         name: json['name'] as String,
         description: json['description'] as String,
-        products:
-            json['products'] != null ? _parseProducts(json['products']) : [],
+        productIds: json['productIds'] != null
+            ? _parseProductIds(json['productIds'])
+            : [],
         image: json['image'] as String?);
   }
 
-  static List<Product> _parseProducts(dynamic productsData) {
-    List productList = productsData as List;
-    List<Product> parsedProducts = [];
+  static List<String> _parseProductIds(dynamic productIdsData) {
+    List idList = productIdsData as List;
+    List<String> parsedIds = [];
 
-    for (var productData in productList) {
-      Map<String, dynamic> productMap = productData as Map<String, dynamic>;
-      Product product = Product.fromJson(productMap);
-      parsedProducts.add(product);
+    for (var id in idList) {
+      parsedIds.add(id as String);
     }
 
-    return parsedProducts;
+    return parsedIds;
   }
-
-  // for search later on
-  List<Product> searchProducts(String query) {
-    final lowerQuery = query.toLowerCase();
-    List<Product> results = [];
-
-    for (Product product in products) {
-      String productName = product.name.toLowerCase();
-      String productDescription = product.description.toLowerCase();
-      bool nameMatch = productName.contains(lowerQuery);
-      bool descriptionMatch = productDescription.contains(lowerQuery);
-
-      if (nameMatch || descriptionMatch) {
-        results.add(product);
-      }
-    }
-
-    return results;
-  }
-
 }
-
-
